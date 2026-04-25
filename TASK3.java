c177505878a504b342b7d4bd8be3d89c98fd80ef
DAWEEENG <abudanza117@gmail.com>
2026-04-25 00:47:23 +0000

    task4

---
+++ TASK3.java
@@ -0,0 +1,20 @@
+public class TASK3 {
+    public static void main(String[] args) {
+
+        String a = new String("WOW");
+        String b = "WOW";
+        String c = b;
+        String d = a;
+
+        boolean b1 = a ==b ;
+        boolean b2 = d.equals(b+"!");
+        boolean b3 = !c.equals(a);
+
+        if(b1&&b2&&b3){
+            System.out.println("success");
+        }
+        else{
+            System.out.println("not equal");
+        }
+    }   
+}
---
+++ TASK4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TASK4 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("INPUT A NAME : " );
+        String name = scan.nextLine();
+        
+        String reversed = new StringBuilder(name).reverse().toString();
+        if (name.equals(reversed)){
+            System.out.println("The input string is a palindrome");
+        }
+            else{
+                System.out.println("The input string is not a palindrome");
+        };
+    }
+}
