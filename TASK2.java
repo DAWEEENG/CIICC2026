8500c72eb4eaf4d835cb359904e9a9c58455efa2
DAWEEENG <abudanza117@gmail.com>
2026-04-25 00:04:40 +0000

    TASK2

---
+++ TASK2.java
@@ -0,0 +1,15 @@
+public class TASK2 {
+
+    static char h = 'H';
+    static int num = 3110;
+    static short one = 1;
+    static byte zero = 0;
+    static char dobulyu = 'w';
+    static float dos = 2.0f;
+    static boolean tama = true;
+
+    public static void main(String[] args) {
+        System.out.println("" + h +num+" "+dobulyu+zero+"r"+one +"d "+dos +" "+tama);
+    }
+
+}
