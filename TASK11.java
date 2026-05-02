a1e552c1c75dcac8694385f6771dbe7b239dd0cc
DAWEEENG <abudanza117@gmail.com>
2026-05-02 18:24:43 +0000

    UPDATED

--- TASK11.java
+++ TASK11.java
@@ -1,3 +1,44 @@
-public class TASK11 {
+class Book {
+    String title;
+    String author;
+    int yearPublished;
+    double price;
 
+    // Constructor
+    public Book(String title, String author, int yearPublished, double price) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.price = price;
+    }
+
+    // toString() method
+    public String toString() {
+        return "Title: \"" + title + "\"\n" +
+               "Author: \"" + author + "\"\n" +
+               "Year Published: " + yearPublished + "\n" +
+               "Price: $" + price;
+    }
+}
+
+public class TASK11 {
+    public static void main(String[] args) {
+
+        // Create 3 Book objects
+        Book book1 = new Book("Java Programming", "John Smith", 2021, 39.99);
+        Book book2 = new Book("Python Basics", "Jane Doe", 2020, 29.99);
+        Book book3 = new Book("C++ Essentials", "Michael Johnson", 2019, 49.99);
+
+        // Print details
+        System.out.println("Book 1:");
+        System.out.println(book1);
+        System.out.println();
+
+        System.out.println("Book 2:");
+        System.out.println(book2);
+        System.out.println();
+
+        System.out.println("Book 3:");
+        System.out.println(book3);
+    }
 }
--- TASK4.java
+++ TASK4.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class TASK4 {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-
-        System.out.print("INPUT A NAME : " );
-        String name = scan.nextLine();
-        
-        String reversed = new StringBuilder(name).reverse().toString();
-        if (name.equals(reversed)){
-            System.out.println("The input string is a palindrome");
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("INPUT A NAME : " );
+            String name = scan.nextLine();
+            
+            String reversed = new StringBuilder(name).reverse().toString();
+            if (name.equals(reversed)){
+                System.out.println("The input string is a palindrome");
+            }
+                else{
+                    System.out.println("The input string is not a palindrome");
+            }
         }
-            else{
-                System.out.println("The input string is not a palindrome");
-        };
+
+        ;
     }
 }
