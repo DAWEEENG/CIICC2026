96396f899e39214cb6bd14fd458b234bfa3ce520
DAWEEENG <abudanza117@gmail.com>
2026-05-02 17:39:08 +0000

    05-03-26

--- TASK8.java
+++ TASK8.java
@@ -1,3 +1,22 @@
 public class TASK8 {
 
+    // Method with variable arguments
+    public static int cumulativeSum(int... numbers) {
+        int totalSum = 0;
+
+        for (int num : numbers) {
+            // Using formula instead of loop
+            int sum = num * (num + 1) / 2;
+
+            System.out.println(num + " = " + sum);
+            totalSum += sum;
+        }
+
+        return totalSum;
+    }
+
+    public static void main(String[] args) {
+        int result = cumulativeSum(4, 5, 10);
+        System.out.println("Total Sum: " + result);      
+    }
 }
--- TASK9.java
+++ TASK9.java
@@ -1,3 +1,39 @@
+import static java.lang.Math.addExact;
+import static java.lang.Math.subtractExact;
+import static java.lang.Math.multiplyExact;
+import static java.lang.Math.floorDiv;
+
 public class TASK9 {
 
+    // Addition
+    public static int add(int a, int b) {
+        return addExact(a, b);
+    }
+
+    // Subtraction
+    public static int subtract(int a, int b) {
+        return subtractExact(a, b);
+    }
+
+    // Multiplication
+    public static int multiply(int a, int b) {
+        return multiplyExact(a, b);
+    }
+
+    // Division
+    public static int divide(int a, int b) {
+        return floorDiv(a, b);
+    }
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 5;
+
+        System.out.println("Addition: " + add(a, b));
+        System.out.println("Subtraction: " + subtract(a, b));
+        System.out.println("Multiplication: " + multiply(a, b));
+        System.out.println("Division: " + divide(a, b));
+
+    }
 }
